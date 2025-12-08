c1e3cc8372fb104f54ca46dcf804f184469c7698
FaolanH <faolanash01@gmail.com>
2025-12-08 04:42:42 +0000

    Attempting to convert the json to a pandas format

--- pfda-assignments/assignment02_bankholiday.py
+++ pfda-assignments/assignment02_bankholiday.py
@@ -8,72 +8,18 @@
 import requests as rq 
 import pandas as pd
 import json
-from collections import Counter
+from pandas import json_normalize
+
 
 url = "https://www.gov.uk/bank-holidays.json"
 response = rq.get(url)
-data = response.json()
-
-# Source: https://www.geeksforgeeks.org/python/response-json-python-requests/
-# https://www.geeksforgeeks.org/python/json-dumps-in-python/
-pretty_data = json.dumps(data, indent=4, sort_keys=True)
-
-event_count = sum(len(event["events"]) for event in data["northern-ireland"])
-print("Number of chapters:", chapter_count)
-# Printing Northern Ireland events
-#for event in data ["northern-ireland"]["events"]:
-#    print (f"{event['title']} on {event['date']}")
-
-#res = list(dict.fromkeys(data))
-#print(res)
+data =  pd.json_normalize(response.json())
 
 
-'''
-    # Printing Northern Ireland events
-for event in data ["northern-ireland"]["events"]:
-    if event in ["northern-ireland"]:
-        unique = False
-        break
-    ["northern-ireland"].add(event)
+#pretty_data = json.dumps(data, indent=4, sort_keys=True)
 
-print (unique)
+#da = json.loads(pretty_data)
 
-        # Check for duplicates
-unique = True
-for x in a:
-    if x in seen:
-        unique = False
-        break
-    seen.add(x)
+#df2 = pd.DataFrame.from_dict(pretty_data, orient ='index')
+print(data)
 
-print(unique)
-
-for event in data["events"]:
-    print(f"Event {event['title']} is {'NI' if division['northern-ireland'] else 'N/A'}")
-
-ni = data["northern-ireland"]
-
-for event in data ["northern-ireland"]:
-    available_products = [e for e in event if e["northern-ireland"]]
-    print(f"Available: {len(available_products)} products")
-# I found a StackOverflow page to help me convert this to a dataframe (https://stackoverflow.com/questions/59306252/importing-json-file-url-to-pandas-data-frame)
-#df_data = pd.DataFrame.from_dict(data['northern-ireland'])
-
-for event in data ["northern-ireland"]["events"]:
-    print (f"{event['title']} on {event['date']}")
-
-
-print(f"Total products: {len(products)}")
-
-# Source: https://www.freecodecamp.org/news/how-to-parse-json-in-python-with-examples/#heading-how-to-parse-json-arrays
-
-for event in data ["northern-ireland"]:
-    status = "Northern Ireland event" if event["northern-ireland"] else "N/A"
-    print (f"{event['title']} on {event['date']}")
-
-#print (list(data.scotland))
-#data = pd.read_json ("https://www.gov.uk/bank-holidays.json")
-
-#pretty_data = data, indent = 4
-#print (pretty_data['northern-ireland']['events'])
-'''
